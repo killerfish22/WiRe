ec1660e2482a3c325197bb7cf955c20f9f0b3446
Dark <dark-knight3@gmx.de>
2015-01-20 21:52:59 +0100

    bla

--- 10thHomework/Exercise3/main.c
+++ 10thHomework/Exercise3/main.c
@@ -3,10 +3,14 @@
 #include <math.h>
 #include "my_matrix.h"
 
+void dtrsv_(char *UPLO, char *TRANS, char *DIAG, INT * N, double *A, INT *LDA, double *RHS, INT *INCX); 
+void dtrsm_(char *SIDE, char *UPLO, char *TRANS, char *DIAG, INT *M, INT *N, double *alpha, double *A, INT *LDA, double *RHS, INT *LDB); 
+
 // Your functions here 
 void scale_col(double s, struct my_matrix_st A, int i, int j, int col);
 void r1_update(struct my_matrix_st A, int i, double s, double *v, int incv, double *w, int incw);
 void LU(struct my_matrix_st A);
+void LU_solve(struct my_matrix_st LU, double *b);
 
 void demo_scale_col(struct my_matrix_st A);
 void demo_r1_update(struct my_matrix_st A);
@@ -14,22 +18,31 @@ void demo_r1_update(struct my_matrix_st A);
 
 int main (int argc , char *argv ){
 	struct my_matrix_st A; 
+	A.LD = 6;
+	A.cols = 6;
+	A.rows = 6;
+	A.values = (double*) malloc(sizeof(double) * 6 * 6);
+	
+	int j;
+	int k;
+	for(j = 0; j < 6; j++)
+		for (k = 0; k < 6; k++)
+			A.values[k + j * A.LD] = 1./(1+k+j);
 
-	// read a dense matrix to A
-	if ( my_matrix_read ("Hamilton.mtx", &A)){
-		fprintf(stderr,"Can not read matrix.\n"); 
-		return 1; 
-	}
 
+	double b[] = {49./20, 223./140, 314./280, 2509./2520, 2131./2520, 20417./27720};
+	
+	LU(A);
+	LU_solve(A, b);
+	
 	int i;
 	for(i = 0; i < 6; i++)
 	{
-		printf("%lg\n", A.values[i]);
+		printf("%lg \n", b[i]);
 	}
 	
-	
 	// Cleanup
-	my_matrix_clear(&A); 
+	//my_matrix_clear(&A); 
 	return 0; 
 
 }
@@ -61,9 +74,19 @@ void LU(struct my_matrix_st A)
 	int k;
 	for(k = 1; k < A.cols; k++)
 	{
-		scale_col(1/A.values[k + k * A.LD], A, k + 1, A.LD, k);
-		r1_update(A, k + 1, -1, &(A.values[(k+1)+(k*A.LD)]), 1 , &(A.values[(k+1)+(k*A.LD)]), A.LD);
+		if (A.values[k + k * A.LD] == 0) printf("Divide by zero\n");
+		scale_col(1./A.values[k + k * A.LD], A, k + 1, A.rows, k);
+		r1_update(A, k + 1, -1, &(A.values[(k+1)+(k*A.LD)]), 1 , &(A.values[(k)+(k+1*A.LD)]), A.LD);
 	}
+}
+
+void LU_solve(struct my_matrix_st LU, double *b)
+{
+	int incv = 1;
+	//Solve Ly = b for y = Ux
+	dtrsv_ ("L", "N", "N", &LU.cols, LU.values, &LU.LD, b, &incv);
+	//Solve Ux = y
+	dtrsv_ ("U", "N", "N", &LU.cols, LU.values, &LU.LD, b, &incv);
 }
 
 void demo_scale_col(struct my_matrix_st A)
@@ -87,23 +110,6 @@ void demo_scale_col(struct my_matrix_st A)
 	  printf("%lg \t\t %lg \n", b[i], A.values[i]);
 	}
 	printf("\n");
-	
-	/*
-	OUTPUT:
-	oldvalue newvalue
-	1 		 2 
-	-0.25 	 -0.5 
-	0 		 0 
-	0 		 0 
-	0 		 0 
-	0 		 0 
-	0 		 0 
-	0 		 0 
-	0 		 0 
-	0 		 0 
-	-0.25 	 -0.5 
-	...
-	*/
 }
 
 void demo_r1_update(struct my_matrix_st A)
@@ -137,16 +143,4 @@ void demo_r1_update(struct my_matrix_st A)
 	  printf("%lg \t %lg \n", b[i], A.values[i + 92 * A.LD]);
 	}
 	printf("\n");
-	
-	/*
-	OUTPUT:
-	oldvalue	newvalue
-	-0.352867 	 5.64713 
-	-0.0198928 	 7.98011 
-	-0.00984245  9.99016 
-	-0.00534529  11.9947 
-	-0.00303501  13.997 
-	-0.00167696  15.9983 
-	-0.000752118 17.9992 
-	*/
 }
